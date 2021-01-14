4bcdf3624a66a13d971f3666672528ccf2deb25e
jdquerales <jquerales72@gmail.com>
2021-01-14 21:03:16 +0000

    Set debug to false

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ print(emp.name)
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
